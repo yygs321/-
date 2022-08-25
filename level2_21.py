4e624c8a6ed3206c1b569ab0b2c52be73d54f4d0
somin <83453597+yygs321@users.noreply.github.com>
2022-08-26 00:21:57 +0900

    [프로그래머스] [Level 2] [다이나믹 프로그래밍] 멀리 뛰기
    [프로그래머스] [Level 2] [다이나믹 프로그래밍] 멀리 뛰기

---
+++ level2_21.py
@@ -0,0 +1,31 @@
+def solution(n):
+    answer = 0
+    arr=[1]*n
+    num=1
+    while len(arr)>=n//2:
+        c=arr.count(2)
+        if c==0 or c==len(arr):
+            answer+=1
+        else:
+            for i in range(1,c+1):
+                num*=len(arr)+1-i
+                num//=i 
+
+            answer+=num
+            num=1
+        
+        if 1 not in arr:
+            break
+        
+        arr.remove(1)
+        for i in range(len(arr)):
+            if arr[i]==1:
+                arr[i]+=1
+            if sum(arr)<n:
+                continue
+            elif sum(arr)==n:
+                break
+        else:
+            break
+        
+    return answer%1234567

4b96baaf09fbba25e970b7c2f1c147e0d01feabf
somin <83453597+yygs321@users.noreply.github.com>
2022-09-21 20:49:57 +0900

    [Level 2] [다이나믹 프로그래밍] 가장 큰 정사각형 찾기
    [Level 2] [다이나믹 프로그래밍] 가장 큰 정사각형 찾기

---
+++ level2_26.py
@@ -0,0 +1,17 @@
+def solution(board):
+    n = len(board)
+    m = len(board[0])
+    
+    # 2중 포문으로 연산
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
+    
+    # 최대 넓이 구하기
+    answer = 0
+    for i in range(n):
+        temp = max(board[i])
+        answer = max(answer, temp)
+    
+    return answer**2
